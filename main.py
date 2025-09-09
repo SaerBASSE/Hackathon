125bd112df00e03b82bcd7a1ca1ac0df058b5e5f
Sir-wb <william.barthelemy@enoc.fr>
2025-09-09 11:29:49 +0200

    ajout traitement des donnés

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('name')
+df = pd.read_csv('')
 df['date'] = pd.to_datetime(df['date'])
 df['year'] = df['date'].dt.year
 df['month'] = df['date'].dt.month   
@@ -14,3 +14,4 @@ df['hours'] = df['date'].dt.hour
 df['minutes'] = df['date'].dt.minute
 
 df_encoded = pd.get_dummies(df, columns=['ENTITY_DESCRIPTION_SHORT'], drop_first=True)
+
