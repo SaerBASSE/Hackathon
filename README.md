fa76d171ed4fc1d85aba2adbe42781995eff873f
Sir-wb <william.barthelemy@enoc.fr>
2025-09-09 11:22:21 +0200

    Transformation donnés

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 hackathon IMI 2025 Saer BASSE, William Barthelemy, Neil Benmouffok, Erwann Cognet
 
 Première idée :
-- Trouver la PCA à l'aide d'une matrice de covariance
+- Trouver la PCA à l'aide d'une matrice de covariance (besoin de transformer )
 - Réaliser une regression linéaire sur ce paramètre
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
+df = pd.read_csv('name')
+df['date'] = pd.to_datetime(df['date'])
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month   
+df['day'] = df['date'].dt.day
+df['hours'] = df['date'].dt.hour
+df['minutes'] = df['date'].dt.minute
+
+df_encoded = pd.get_dummies(df, columns=['ENTITY_DESCRIPTION_SHORT'], drop_first=True)
